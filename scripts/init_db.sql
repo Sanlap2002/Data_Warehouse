520c0fd2f53f9d89d2d3e8ec2bac577dd9c9bb56
Sanlap2002 <112051909+Sanlap2002@users.noreply.github.com>
2025-06-08 00:44:40 +0530

    Update init_db.sql

--- scripts/init_db.sql
+++ scripts/init_db.sql
@@ -1,1 +1,22 @@
+/*This script initialises the database "Data WArehouse
+It first checks if there's any such database named Data Warehouse and drops it
+it does so safely by forcing the database into single user mode and disconnecting all other users and rolling back their transactions.
+*/
 
+
+USE master;
+GO
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'Data_Warehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+CREATE DATABASE Data_Warehouse;
+USE Data_Warehouse;
+GO
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
